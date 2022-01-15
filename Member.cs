eb8fcc4da71527a95f90f12986604d87cad3a9ce
blxcklght112 <91447232+blxcklght112@users.noreply.github.com>
2022-01-16 00:10:57 +0700

    Update Member.cs

--- Member.cs
+++ Member.cs
@@ -4,13 +4,13 @@ namespace CS_Day1
 {
     public class Member
     {
-        public String FirstName{get;set;}
-        public String LastName{get;set;}
-        public String Gender{get;set;}
-        public DateTime Birth{get;set;}
-        public String PhoneNumber{get;set;}
-        public String BirthPlace{get;set;}
-        public int Age{get{return DateTime.Now.Year - Birth.Year;}}
-        public bool IsGraduated;
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Gender { get; set; }
+        public DateTime Birthday { get; set; }
+        public String PhoneNumber { get; set; }
+        public String BirthPlace { get; set; }
+        public int Age { get { return DateTime.Now.Year - Birthday.Year; } }
+        public bool IsGraduated { get; set; }
     }
 }
